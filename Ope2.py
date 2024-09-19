5dadfcb969ddf06809c2ea147380484a74562a1c
Jean <jeancarloac60@gmail.com>
2024-09-18 23:43:09 -0500

    commit inicial

---
+++ Ope2.py
@@ -0,0 +1,9 @@
+# Solicitar al usuario que ingrese dos números
+num1 = float(input("Ingresa el primer número: "))
+num2 = float(input("Ingresa el segundo número: "))
+
+# Realizar la multiplicación
+resultado = num1 * num2
+
+# Mostrar el resultado
+print("La multiplicación de los dos números es:", resultado)
---
+++ Operacion.py
@@ -0,0 +1,9 @@
+# Solicitar al usuario que ingrese dos números
+num1 = float(input("Ingresa el primer número: "))
+num2 = float(input("Ingresa el segundo número: "))
+
+# Realizar la suma
+resultado = num1 + num2
+
+# Mostrar el resultado
+print("La suma de los dos números es:", resultado)
